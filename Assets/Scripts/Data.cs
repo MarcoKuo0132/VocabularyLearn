c841993bdd2f81fa6ffd9ea0b9960f4b07a8f5dd
MarcoKuo0132 <MarcoKuo0132@github.com>
2020-06-10 23:29:00 +0800

    finish main scripts

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -2,17 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Data : MonoBehaviour
+public class Data
 {
-    // Start is called before the first frame update
-    void Start()
+    public struct VocabularyBase
     {
-        
+        public string Vocabulary_en;   //英文單字
+        public string Vocabulary_tw;   //中文意思
+        public string Sentence_en;     //英文句子
+        public string Sentence_tw;     //中文意思
+        public string Synonyms;     //同義字
+        public string Antonym;      //反義字
+        public string Extension_en;    //英文延伸字
+        public string Extension_tw;    //中文意思
     }
 
-    // Update is called once per frame
-    void Update()
+    public List<VocabularyBase> VocabularysList;
+
+    public void setVocabularysArray()
     {
-        
+        VocabularysList = new List<VocabularyBase>();
+
+        VocabularysList.Add( new VocabularyBase 
+            { 
+                Vocabulary_en = "a/an",
+                Vocabulary_tw = "一個",
+                Sentence_en = null,
+                Synonyms = null,
+                Antonym = null,
+                Extension_en = null 
+            }
+        );
+
+        VocabularysList.Add( new VocabularyBase
+            {
+                Vocabulary_en = "able",
+                Vocabulary_tw = "能夠的",
+                Sentence_en = "Nobody is able to finish the math test.",
+                Sentence_tw = "沒人能夠完成這次數學考試。",
+                Synonyms = "be able to",
+                Antonym = null,
+                Extension_en = null
+            }
+        );
+
+        VocabularysList.Add( new VocabularyBase
+            {
+                Vocabulary_en = "about",
+                Vocabulary_tw = "關於",
+                Sentence_en = null,
+                Synonyms = "concerning",
+                Antonym = null,
+                Extension_en = null
+            }
+        );
+
+        VocabularysList.Add( new VocabularyBase
+            {
+                Vocabulary_en = "above",
+                Vocabulary_tw = "在...之上",
+                Sentence_en = "A pigeon is flying above the house.",
+                Sentence_tw = "一隻鴿子正在房子上面飛。",
+                Synonyms = "concerning",
+                Antonym = null,
+                Extension_en = null
+            }
+        );
+
+        VocabularysList.Add( new VocabularyBase
+            {
+                Vocabulary_en = "according to",
+                Vocabulary_tw = "根據",
+                Sentence_en = null,
+                Synonyms = "on the basis of",
+                Antonym = null,
+                Extension_en = null
+            }
+        );
     }
 }
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,18 +1,184 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Main : MonoBehaviour
 {
+    public GameObject VocabularyInput;
+    public GameObject SentenceInput;
+    public GameObject SynonymsInput;
+    public GameObject AntonymInput;
+    public GameObject ExtensionInput;
+
+    public GameObject VocabularyText;
+    public GameObject SentenceText;
+    public GameObject SynonymsText;
+    public GameObject AntonymText;
+    public GameObject ExtensionText;
+
+    Data data;
+
+    enum appState
+    {
+        wait,
+        input,
+        waitForInput,
+        check
+    };
+    appState state;
+    int count;
+
+    bool isPass;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        data = new Data();
+        data.setVocabularysArray();
+
+        state = appState.wait;
+        count = 0;
+        isPass = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (state)
+        {
+            case appState.wait:
+                break;
+            case appState.input:
+                SetText();
+                break;
+            case appState.waitForInput:
+                break;
+            case appState.check:
+                Check();
+                break;
+        }
+    }
+
+    public void PressStart()
+    {
+        state = appState.input;
+    }
+
+    public void PressSubmit()
+    {
+        state = appState.check;
+    }
+
+    void SetText()
+    {
+        if (data.VocabularysList[count].Vocabulary_en != null)
+        {
+            VocabularyText.GetComponent<Text>().text = data.VocabularysList[count].Vocabulary_tw;
+        }
+
+        if (data.VocabularysList[count].Sentence_en != null)
+        {
+            SentenceText.GetComponent<Text>().text = data.VocabularysList[count].Sentence_tw;
+        }
+
+        if (data.VocabularysList[count].Synonyms != null)
+        {
+            SynonymsText.GetComponent<Text>().text = data.VocabularysList[count].Vocabulary_tw;
+        }
+
+        if (data.VocabularysList[count].Antonym != null)
+        {
+            AntonymText.GetComponent<Text>().text = "有反義字";
+        }
+
+        if (data.VocabularysList[count].Extension_en != null)
+        {
+            ExtensionText.GetComponent<Text>().text = data.VocabularysList[count].Extension_tw;
+        }
+
+        state = appState.waitForInput;
+    }
+
+    void Check()
+    {
+        if (data.VocabularysList[count].Vocabulary_en != null)
+        {
+            if (VocabularyInput.GetComponent<InputField>().text != (data.VocabularysList[count].Vocabulary_en))
+            {
+                Wrong();
+                return;
+            }
+        }
+
+        if (data.VocabularysList[count].Sentence_en != null)
+        {
+            if (SentenceInput.GetComponent<InputField>().text != (data.VocabularysList[count].Sentence_en))
+            {
+                Wrong();
+                return;
+            }
+        }
+
+        if (data.VocabularysList[count].Synonyms != null)
+        {
+            if (SynonymsInput.GetComponent<InputField>().text != (data.VocabularysList[count].Synonyms))
+            {
+                Wrong();
+                return;
+            }
+        }
+
+        if (data.VocabularysList[count].Antonym != null)
+        {
+            if (AntonymInput.GetComponent<InputField>().text != (data.VocabularysList[count].Antonym))
+            {
+                Wrong();
+                return;
+            }
+        }
+
+        if (data.VocabularysList[count].Extension_en != null)
+        {
+            if (ExtensionInput.GetComponent<InputField>().text != (data.VocabularysList[count].Extension_en))
+            {
+                Wrong();
+                return;
+            }
+        }
+
+        Pass();
+    }
+
+    void Wrong()
+    {
+        CleanAllInput();
+        state = appState.waitForInput;
+    }
+
+    void Pass()
+    {
+        CleanAllInput();
+        CleanAllText();
+        count++;
+        state = appState.input;
+    }
+
+    void CleanAllInput()
+    {
+        VocabularyInput.GetComponent<InputField>().text = "";
+        SentenceInput.GetComponent<InputField>().text = "";
+        SynonymsInput.GetComponent<InputField>().text = "";
+        AntonymInput.GetComponent<InputField>().text = "";
+        ExtensionInput.GetComponent<InputField>().text = "";
+    }
+
+    void CleanAllText()
+    {
+        VocabularyText.GetComponent<Text>().text = "";
+        SentenceText.GetComponent<Text>().text = "";
+        SynonymsText.GetComponent<Text>().text = "";
+        AntonymText.GetComponent<Text>().text = "";
+        ExtensionText.GetComponent<Text>().text = "";
     }
 }
